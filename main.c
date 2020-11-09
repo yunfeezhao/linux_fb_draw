e7890b79211485228113a38a6e04b4c5aa139c98
赵云飞 <64369966+yunfeezhao@users.noreply.github.com>
2020-11-09 10:38:32 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include "lcd.h"
-
+int type=0;
 int main()
 {
+	int i=0,k=0;
 	initset();
 	clear();
 	setbkcolor(0, 0, 0);
-//	getchar();
-	setcolor(255, 0, 0);
-//	draw_circle(512, 300, 200);
-//	getchar();
-//	rect(200, 200, 824, 400);
-//	getchar();
-	printlcd(200, 200, "Hello, NJU!");
 	getchar();
+	setcolor(255, 0, 0);
+	
+	
+	while(1){
+	clear();
+	circle(640+i, 360+k, 50);
+	circle(640+i, 360-k, 50);
+	circle(640-i, 360+k, 50);
+	circle(640-i, 360-k, 50);
+	show();
+
+	if(i>590)i=0,k=0;
+	else if(i>=310) --k,++i;
+	else k++,i++;
+	usleep(10000);
+	if(type==1) break;
+	}
+	
+	rect(200, 200, 824, 400);
+	printlcd(200, 200, "Hello, NJU!");
 	display_bmp("a1.bmp");
 	getchar();
 	return 0;
